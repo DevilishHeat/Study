f3b4a263c3e200aa1f04645bf9618cf55082f981
Vladimir Markov <markov.vov@gmail.com>
2025-05-13 22:33:49 +0700

    lab3

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -13,9 +13,7 @@ class IndexController extends Controller
     public function actionLab3()
     {
         $model = new Lab3Model();
-        VarDumper::dump(Yii::$app->request->post('Lab3Model'));
-        exit();
-        if ($model->load(Yii::$app->request->post('Lab3Model'))) {
+        if ($model->load(Yii::$app->request->post('Lab3Model'), '')) {
             $model->encode();
         }
 
@@ -29,7 +27,7 @@ class IndexController extends Controller
     public function actionLab3Decode(): string
     {
         $model = new Lab3Model();
-        if ($model->load(Yii::$app->request->post('Lab3Model'))) {
+        if ($model->load(Yii::$app->request->post('Lab3Model'), '')) {
             $model->decode();
         }
 
