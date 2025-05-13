7ed0e5abd4f42faccf6265002339540e874cfb38
Vladimir Markov <markov.vov@gmail.com>
2025-05-13 22:07:17 +0700

    lab3

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -14,6 +14,7 @@ class IndexController extends Controller
     {
         $model = new Lab3Model();
         VarDumper::dump(Yii::$app->request->post());
+        exit();
         if ($model->load(Yii::$app->request->post())) {
             $model->encode();
         }
