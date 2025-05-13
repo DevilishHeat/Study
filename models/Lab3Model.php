9ecefad5c11176a3a5b612a25a47976d26520a18
Vladimir Markov <markov.vov@gmail.com>
2025-05-13 22:17:02 +0700

    lab3

--- models/Lab3Model.php
+++ models/Lab3Model.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use yii\base\Model;
+use yii\helpers\VarDumper;
 
 class Lab3Model extends Model
 {
@@ -15,16 +16,15 @@ class Lab3Model extends Model
         $lastLetterCode = mb_ord('z');
         $keyChar = array_map(fn ($char) => mb_ord($char) - $firstLetterCode, str_split($this->key));
         $keyLength = strlen($this->key);
-        $textChars = str_split($this->decoded, $keyLength);
+        $textChunks = str_split($this->decoded, $keyLength);
         $result = '';
-        foreach ($textChars as $textChar) {
-            $textChar = str_split($textChar);
-            foreach ($textChar as $j => $char) {
-                if (($newCharCode = mb_ord($char) + $keyChar[$j]) <= $lastLetterCode) {
-                    $newChar = mb_chr($newCharCode);
-                } else {
-                    $newChar = mb_chr($newCharCode - 26);
-                }
+        VarDumper::dump($textChunks);
+        exit();
+        foreach ($textChunks as $textChunk) {
+            $textChunk = str_split($textChunk);
+            foreach ($textChunk as $j => $char) {
+                $newCharCode = mb_ord($char) + $keyChar[$j];
+                $newChar = $newCharCode <= $lastLetterCode ? mb_chr($newCharCode) : mb_chr($newCharCode - 26);
                 $result .= $newChar;
             }
         }
@@ -37,16 +37,13 @@ class Lab3Model extends Model
         $firstLetterCode = mb_ord('a');
         $keyChar = array_map(fn ($char) => mb_ord($char) - $firstLetterCode, str_split($this->key));
         $keyLength = strlen($this->key);
-        $textChars = str_split($this->encoded, $keyLength);
+        $textChunks = str_split($this->encoded, $keyLength);
         $result = '';
-        foreach ($textChars as $textChar) {
-            $textChar = str_split($textChar);
-            foreach ($textChar as $j => $char) {
-                if (($newCharCode = mb_ord($char) - $keyChar[$j]) >= $firstLetterCode) {
-                    $newChar = mb_chr($newCharCode);
-                } else {
-                    $newChar = mb_chr($newCharCode + 26);
-                }
+        foreach ($textChunks as $textChunk) {
+            $textChunk = str_split($textChunk);
+            foreach ($textChunk as $j => $char) {
+                $newCharCode = mb_ord($char) - $keyChar[$j];
+                $newChar = $newCharCode >= $firstLetterCode ? mb_chr($newCharCode) : mb_chr($newCharCode + 26);
                 $result .= $newChar;
             }
         }
