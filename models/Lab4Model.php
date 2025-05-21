2225704c91c2110c9360eb8a9b92fa8b9ca583b4
vladimir <markov.vov@gmail.com>
2025-05-21 17:33:03 +0700

    lab4

---
+++ models/Lab4Model.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+
+class Lab4Model extends Model
+{
+    private array $squire = [
+        16, 3, 2, 13,
+        5, 10, 11, 8,
+        9, 6, 7, 12,
+        4, 15, 14, 1,
+    ];
+    private ?array $squireIndexes = null;
+    private ?int $numberOfElements = null;
+
+    public ?string $decoded;
+    public ?string $encoded;
+
+    public function rules()
+    {
+        return [
+            [['encoded', 'decoded'], 'string', 'max' => $this->numberOfElements],
+        ];
+    }
+
+    public function init()
+    {
+        $this->squireIndexes = array_flip($this->squire);
+        $this->numberOfElements = count($this->squire);
+    }
+
+    public function encode(): void
+    {
+        $letters = mb_str_split($this->decoded);
+
+        $indexes = [];
+        foreach ($letters as $i => $letter) {
+            $indexes[$this->squireIndexes[$i + 1]] = $letter;
+        }
+
+        $result = '';
+        foreach ($this->squire as $item) {
+            $result .=
+        }
+
+        $this->encoded = $result;
+    }
+
+    public function decode(): void
+    {
+        $letters = mb_str_split($this->encoded);
+
+        $result = [];
+        foreach ($letters as $i => $letter) {
+            $result[$this->squireIndexes[$i + 1]] = $letter;
+        }
+        $resultString = '';
+        foreach ($result as $pair) {
+            $resultString .= $this->squire[$pair['index']];
+        }
+
+        $this->decoded = $resultString;
+    }
+}
